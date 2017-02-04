5df05e271fe893d9e91e5379f17328d3707f996e
Jean-Luc CANELA <jlcanelam@gmail.com>
2017-02-05 00:47:54 +0100

    workaround to enable watch:test  + debugging

--- test/unit/httpTest.js
+++ test/unit/httpTest.js
@@ -8,9 +8,19 @@ let server = require('../../src/server')
 chai.use(chaiHttp)
 
 describe('Server', () => {
+  before((done) => {
+    // TODO: cleanup connection directly in server ...
+    const mongoose = require('mongoose')
+    mongoose.schemas = {}
+    mongoose.models = {}
+    mongoose.connections.forEach((cn) => cn.close())
+    done()
+  })
+  
   beforeEach((done) => {
     done()
   })
+
   describe('GET /', () => {
     it('it should GET home page', (done) => {
       chai.request(server)
